ab783a486c839064da9279fdb5afe4f97fd69de0
Marcus <marcusfonseca15@gmail.com>
2025-03-24 21:38:56 -0300

    Timer regressivo

---
+++ sabotage_station/src/mainGame/GameTimer.java
@@ -0,0 +1,29 @@
+package mainGame;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameTimer {
+    private Timer timer;
+    private int count;
+
+    public GameTimer(int seconds) {
+        this.count = segs;
+        this.timer = new Timer();
+    }
+
+    public void iniciar() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(count);
+                count--;
+                if (count < 0) {
+                    timer.cancel();
+                }
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 0, 1000); // task, delay e periodo
+    }
+}
---
+++ sabotage_station/src/mainGame/MainGame.java
@@ -0,0 +1,11 @@
+package mainGame;
+
+import mainGame.GameTimer;
+
+//CLASSE DA TELA PRINCIPAL (INTERFACE PERMANCE EM CAMADA SUPERIOR, LEVEIS SÃO APRESENTADAS AQUI)
+public class MainGame {
+    public static void main(String[] args) {
+        GameTimer gameTimer = new GameTimer(10);
+        gameTimer.iniciar();
+    }
+}
