f01a5853c7fd37950b61ab1f4d46f6b1672eb7a4
Marcus <marcusfonseca15@gmail.com>
2025-03-27 16:44:43 -0300

    feat: Level Loader base (Prox: Criar abstract class Objeto e aplicar)

---
+++ sabotage_station/LevelLoader/LevelLoader.java
@@ -0,0 +1,54 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class LevelLoader {
+
+    // ---------> LEITOR DA IMAGEM <------
+    public static void main(String[] args) {
+
+        // carrega img da fase
+        BufferedImage img = ImageIO.read(new File("fase.png"));
+
+        int T_SIZE = 50;
+        int lin = img.getHeight() / T_SIZE;
+        int col = img.getWidth() / T_SIZE;
+
+        for (int y = 0; y < lin; y++) {
+            for (int x = 0; x < col; x++) {
+
+                // Pegar a cor do CENTRO do tile
+                int px = (x * T_SIZE) + (T_SIZE / 2);
+                int py = (y * T_SIZE) + (T_SIZE / 2);
+
+                // Pega a cor do pixel
+                Color color = new Color(img.getRGB(px, py));
+            }
+        }
+
+    }
+
+    // Relacionar a cor com um objeto
+    public static String corToObj(Color color) {
+        // PRETO = VAZIO
+        if (color.equals(new Color(0, 0, 0))) {
+            return "Vazio";
+            // AZUL = BARREIRA/PCHÃO/PAREDE
+        } else if (color.equals(new Color(0, 0, 225))) {
+            return "Barreira";
+            // VERMELHO = LASER
+        } else if (color.equals(new Color(225, 0, 0))) {
+            return "Laser";
+            // VERDE = PISTÃO
+        } else if (color.equals(new Color(0, 225, 0))) {
+            return "Pistão";
+            // AMARELO = PORTA
+        } else if (color.equals(new Color(225, 225, 0))) {
+            return "Porta";
+        }
+        // Fora desse espectro não existe
+        return "NE";
+    }
+
+}
